e6789de63aea8f05b9131a8d47b8e78e04b5a462
Nicolas <92864789+Nicolass29@users.noreply.github.com>
2024-10-28 15:15:27 -0300

    Update grafico.py

--- grafico.py
+++ grafico.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 
 
 def gerar_grafico(df, caminho_imagem):
-    # Converter o valor recebido para float se necessário
+    # Converte a coluna 'Valor recebido' para float se necessário
     df['Valor recebido'] = df['Valor recebido'].replace(
         {r'R\$': '', r'\.': '', ',': '.'}, regex=True).astype(float)
 
-    # Criando uma figura
+    # Criação da figura do gráfico
     fig, ax = plt.subplots(figsize=(10, 6))
 
-    # Definindo a largura das barras
+    # Largura das barras
     bar_width = 0.2
 
     # Posições das barras no eixo X
@@ -24,21 +24,19 @@ def gerar_grafico(df, caminho_imagem):
     ax.bar(clientes + bar_width, df['Valor recebido'] / 1000000, width=bar_width,
            color='g', alpha=0.6, label='R$')
 
-    # Configurando os rótulos do eixo X com os nomes dos clientes
-    # Posiciona os rótulos dos clientes no centro das barras
+    # Configurações do eixo X com os nomes dos clientes
     ax.set_xticks(clientes + bar_width / 2)
-    # Substitui os valores numéricos pelos nomes dos clientes
     ax.set_xticklabels(df['Clientes'])
 
-    # Definindo o título e os rótulos dos eixos
+    # Definições do título e rótulos dos eixos
     ax.set_title('Comparação de Horas Gastas e Valor Recebido por Cliente')
     ax.set_xlabel("Clientes")
     ax.set_ylabel("Valores em Horas/10000 e R$/1000000")
 
-    # Adicionando legenda
+    # Adiciona legenda
     ax.legend(loc='upper left')
 
-    # Salvando o gráfico e fechando a figura
+    # Salva o gráfico e fecha a figura
     plt.savefig(caminho_imagem, format='png')
-    plt.show()  # Opcional: mostra o gráfico no console
+    plt.show()  # Opcional: exibe o gráfico
     plt.close(fig)  # Fecha a figura após salvar
