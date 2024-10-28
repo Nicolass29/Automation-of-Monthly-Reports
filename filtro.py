4d9877027145f91cd08b986c848535387470a5fd
Nicolas <92864789+Nicolass29@users.noreply.github.com>
2024-10-28 15:16:10 -0300

    Update filtro.py

--- filtro.py
+++ filtro.py
@@ -2,17 +2,24 @@ import pandas as pd
 
 
 def gerar_relatorio(caminho_entrada, caminho_saida):
+    # Lê os dados do arquivo Excel
     dados = pd.read_excel(caminho_entrada)
+
+    # Converte a coluna 'Valor recebido' de string para float para cálculos
     dados['Valor recebido'] = dados['Valor recebido'].replace(
         {r'R\$': '', r'\.': '', ',': '.'}, regex=True).astype(float)
 
+    # Agrupa os dados por cliente e calcula totais e média de satisfação
     relatorio = dados.groupby('Clientes').agg(
-        {'Horas gastas': 'sum', 'Valor recebido': 'sum',
-            'Satisfacao': lambda x: round(x.mean())}
+        {'Horas gastas': 'sum',
+         'Valor recebido': 'sum',
+         'Satisfacao': lambda x: round(x.mean())}
     ).reset_index()
 
+    # Formata a coluna 'Valor recebido' de volta para string com o formato monetário
     relatorio['Valor recebido'] = relatorio['Valor recebido'].apply(
         lambda x: f'R${x:,.2f}'.replace(',', 'X').replace('.', ',').replace('X', '.'))
 
+    # Salva o relatório consolidado em um novo arquivo Excel
     relatorio.to_excel(caminho_saida, index=False)
     return relatorio
